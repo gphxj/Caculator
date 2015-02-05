dfac85b85ca08fe5936423ccae2528bdb671c4aa
gphxj <gph_xj@163.com>
2015-02-05 18:57:18 +0800

    Caulator commit

---
+++ Caculator/CalculatorBrain.swift
@@ -0,0 +1,78 @@
+//
+//  CalculatorBrain.swift
+//  Caculator
+//
+//  Created by gph on 15/2/3.
+//  Copyright (c) 2015年 gph. All rights reserved.
+//
+
+import Foundation
+
+//Clculator Brain 计算器的大脑
+class ClculatorBrain
+{
+    private enum Op {   // Op 运算、操作
+        case Operand(Double)    //操作数
+        case UnaryOperation(String, Double -> Double)   //一元运算
+        case BinaryOperation(String, (Double, Double) -> Double)    //二元运算
+    }
+    
+    private var opStack = [Op]()    //操作栈
+    
+    private var knownOps = [String:Op]()    //已知的运算
+    
+    init() {
+        knownOps["×"] = Op.BinaryOperation("×", *)          // 等同于 ("×") { $0 * $1 }
+        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)          // 等同于 ("+") { $0 + $1 }
+        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)        // 等同于 ("√") { sqrt($0) }
+        
+
+    }
+
+//  evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) 参数：result 结果，remainingOps 剩下的操作
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op])
+    {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+//  evaluate() 求…的数值
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        return result
+    }
+
+//  pushOperand(operand: Double)    把操作数压入堆栈
+    func pushOperand(operand: Double) {
+        opStack.append(Op.Operand(operand))
+    }
+    
+//  performOperation(symbol: String)    执行操作
+    func performOperation(symbol: String) {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+    }
+}
--- Caculator/ViewController.swift
+++ Caculator/ViewController.swift
@@ -10,16 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if  userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+//        println("digit = \(digit)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+            case "×": performOperation{ $0 * $1 }
+            case "÷": performOperation{ $1 / $0 }
+            case "+": performOperation{ $0 + $1 }
+            case "−": performOperation{ $1 - $0 }
+            case "√": performOperation{ sqrt($0) }
+            default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
     }
 
-
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
